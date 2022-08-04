837aaa70ac32ca0f7b51a5d57ce11553df666dfe
MohaDevel <mohalhakami2019@gmail.com>
2022-08-04 22:32:48 +0300

    this is git data with admins

---
+++ productsadmin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+	<meta charset="utf-8">
+	  <!--This is an introduction to the site -->
+	<meta name="description" content="Online marketing site where you
+	 can buy the best products ">
+
+	  <!-- This is the bootstrap  link-->
+      <link rel="stylesheet" type="text/css" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+
+	  <!-- This is the style link-->
+	<link rel="stylesheet" type="text/css" href="css/productsadmincss.css">
+	<title>The sc admin interface</title>
+</head>
+<body>
+
+	<div class="container"> 
+		<h3 class="title">All products available</h3>
+
+		<div class="product" date-name="p-1">
+			<img src="......" alt="" >
+			<h3></h3>
+			<div class="price"> </div>
+		</div>
+		
+	 
+
+
+	</div>
+
+</body>
+</html>
